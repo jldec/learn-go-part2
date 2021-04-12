607826faaa8c4433923509e0646496b9fab7eaaa
Jurgen Leschner <jldec@ciaosoft.com>
2021-04-12 17:13:50 +0100

    Serve from STATIC_DIR env or .

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"runtime"
 
 	"github.com/jldec/learn-go-part2/counter"
@@ -15,11 +16,20 @@ func main() {
 	cnt := counter.New()
 	http.Handle("/counter", cnt.Handler())
 	http.Handle("/sqrt", sqrt.Handler(cnt))
-	http.Handle("/", static.Handler(cnt, "/Users/jleschner/pub/fmc/generated/"))
+	http.Handle("/", static.Handler(cnt, staticDir()))
 
 	log.Println("Listening on :3000 (maxprocs: " + fmt.Sprint(runtime.GOMAXPROCS(0)) + ")")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func staticDir() string {
+	envDir := os.Getenv("STATIC_DIR")
+	if envDir != "" {
+		return envDir
+	}
+	// TODO: check windows
+	return "."
 }
